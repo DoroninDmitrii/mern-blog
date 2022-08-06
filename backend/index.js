b3c3d4472087e16600e0c88a360e33b866baa4c7
Dmitrii Doronin <doronin_dima@mail.ru>
2022-08-06 11:09:02 +0300

    send request used compass

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from 'bcrypt';
 import mongoose from "mongoose";
 import { registerValidation } from './validation/auth.js';
 import { validationResult } from "express-validator";
+
+import UserSchema from './models/user.js'
 
 mongoose.connect(
   'mongodb+srv://admin:111@cluster0.edvx7.mongodb.net/?retryWrites=true&w=majority'
@@ -10,18 +13,29 @@ mongoose.connect(
 .catch((err) => console.log('Database is not working', err));
 
 const app = express();
+app.use(express.json());
 
-app.use(express.json())
-
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array())
   } else {
-    return res.json({
-      success: true
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const userData = new UserSchema({
+      email: req.body.email,
+      passwordHash,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
     })
+
+    const user = await userData.save();
+
+    return res.json(user);
   }
 });
 
