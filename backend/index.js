64319dc0a30217d877af4510e271fd3f79b2d3be
Dmitrii Doronin <doronin_dima@mail.ru>
2022-08-18 19:17:57 +0300

    multer

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import multer from "multer";
 import mongoose from "mongoose";
 import { registerValidation, loginValidation, postCreateValidation } from './validation.js';
 
@@ -13,11 +14,29 @@ mongoose.connect(
   .catch((err) => console.log('Database is not working', err));
 
 const app = express();
+
+const storage = multer.diskStorage({
+  destination: function (req, res, cb) {
+    cb(null, 'uploads');
+  }, 
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage });
+
 app.use(express.json());
 
 app.post('/auth/login', loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`
+  })
+})
 
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
 app.get('/posts', PostController.getAll);
