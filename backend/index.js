7c0bffc2fd92f4069fab991e5d1181a7b8d21151
Dmitrii Doronin <doronin_dima@mail.ru>
2022-08-24 20:01:05 +0300

    load data

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 const storage = multer.diskStorage({
   destination: function (req, res, cb) {
     cb(null, 'uploads');
-  }, 
+  },
   filename: (req, file, cb) => {
     cb(null, file.originalname)
   }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,8 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Grid from '@mui/material/Grid';
 
-import axios from '../axios';
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchPosts } from '../redux/slices/posts';
 
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
@@ -11,8 +12,13 @@ import { CommentsBlock } from '../components/CommentsBlock';
 
 export const Home = () => {
 
+  const dispatch = useDispatch();
+  const {posts, tags} = useSelector(state => state.posts);
+
+  const isPostLoading = posts.status === 'loading';
+
   useEffect(() => {
-    axios.get('/posts')
+    dispatch(fetchPosts())
   }, [])
   return (
     <>
@@ -22,10 +28,12 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {[...Array(5)].map(() => (
+          {isPostLoading ? [...Array(5)] : posts.items.map((el, index) => isPostLoading ? (
+            <Post key={index} isLoading={true}/>
+          ) : (
             <Post
-              id={1}
-              title="Roast the code #1 | Rock Paper Scissors"
+              id={el._id}
+              title={el.title}
               imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
               user={{
                 avatarUrl:
