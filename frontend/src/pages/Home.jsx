594598db60a3a73cea781afa822ac1981bdb3ade
Dmitrii Doronin <doronin_dima@mail.ru>
2022-08-26 18:54:35 +0300

    fill comp Home by data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -35,15 +35,11 @@ export const Home = () => {
               id={el._id}
               title={el.title}
               imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-              user={{
-                avatarUrl:
-                  'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                fullName: 'Keff',
-              }}
-              createdAt={'12 июня 2022 г.'}
-              viewsCount={150}
+              user={el.user}
+              createdAt={el.createdAt}
+              viewsCount={el.viewsCount}
               commentsCount={3}
-              tags={['react', 'fun', 'typescript']}
+              tags={el.tags}
               isEditable
             />
           ))}
