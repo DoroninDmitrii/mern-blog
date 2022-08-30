b92143ffa6e71188c5e4574560bfa677d23356b7
Dmitrii Doronin <doronin_dima@mail.ru>
2022-08-30 20:16:01 +0300

    login is ready

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -3,26 +3,57 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import {useForm} from 'react-hook-form'
 
 import styles from "./Login.module.scss";
+import { useDispatch } from "react-redux";
+import { fetchAuth } from "../../redux/slices/authSlice";
 
 export const Login = () => {
+  const {register, 
+    handleSubmit, 
+    setError, 
+    formState: {errors, isValid}}
+     = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+
+  const dispatch = useDispatch()
+
+  const onSubmit = (values) => {
+    dispatch(fetchAuth(values));
+  }
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
       <TextField
+      {...register('email', {required: 'Write your email'})}
         className={styles.field}
         label="E-Mail"
-        error
-        helperText="Неверно указана почта"
+        error={Boolean(errors.email?.message)}
+        type='email'
+        helperText={errors.email?.message}
         fullWidth
       />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
+      <TextField
+      {...register('password', {required: 'Write your password'})} 
+      className={styles.field} 
+      label="Пароль" fullWidth
+      error={Boolean(errors.password?.message)}
+      helperText={errors.password?.message} 
+      />
+      <Button type='submit' size="large" variant="contained" fullWidth>
         Войти
       </Button>
+      </form>
     </Paper>
   );
 };
